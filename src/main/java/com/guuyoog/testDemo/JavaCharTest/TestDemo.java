96594019649f9181795799d39233cf331fd488cd
guiyong <guiyong@duolala.com.cn>
2017-12-27 17:25:04 +0800

    java八种基本类型说明

---
+++ src/main/java/com/guuyoog/testDemo/JavaCharTest/TestDemo.java
@@ -0,0 +1,27 @@
+package com.guuyoog.testDemo.JavaCharTest;
+
+/**
+ * @author Guy
+ * @create 2017/12/27  17:18
+ **/
+public class TestDemo {
+
+    public static void main(String[] args) {
+        // 0
+        char a1 = 48;
+        char a2 = 'a';
+        char a3 = 'A';
+        char a4 = 97;
+        char a5 = 65;
+        char a6 = 'a' + 1;
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+        System.out.println(a4);
+        System.out.println(a5);
+        System.out.println(a6);
+        System.out.println(a2 + a3);
+        System.out.println(a4 + a5);
+    }
+
+}
